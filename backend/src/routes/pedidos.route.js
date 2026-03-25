036834f1be5f05ef57b00e2290626d8fe0fb622c
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-03-25 19:15:55 +0100

    feat: añadir rutas para marcar pedidos como fabricados y obtener historial de pedidos por operario, mejorando la funcionalidad de gestión de pedidos

--- backend/src/routes/pedidos.route.js
+++ backend/src/routes/pedidos.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getPedidos, getPedidosByEmpresa, getPedidosByOperario, deletePedido } from '../controllers/pedidos.controller.js';
+import { getPedidos, getPedidosByEmpresa, getPedidosByOperario, deletePedido, marcarComoFabricado, getPedidosHistorialByOperario } from '../controllers/pedidos.controller.js';
 
 const router = Router();
 
@@ -7,5 +7,7 @@ router.get('/pedidos', getPedidos);
 router.get('/pedidos/empresa/:id', getPedidosByEmpresa);
 router.get('/pedidos/operario/:id', getPedidosByOperario);
 router.delete('/pedidos/:id', deletePedido);
+router.put('/pedidos/marcar-fabricado/:id', marcarComoFabricado);
+router.get('/pedidos/historial/operario/:id', getPedidosHistorialByOperario);
 
 export { router as pedidosRoutes };
