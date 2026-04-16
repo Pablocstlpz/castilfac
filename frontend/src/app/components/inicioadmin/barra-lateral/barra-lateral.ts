5be5f0791bd06b89296b89bc2d96bef1ef986850
Pablo M. Del Castillo López <pmcastillo@deuser.es>
2026-04-16 14:27:22 +0200

    feat: agregar componente de barra lateral para la administración

---
+++ frontend/src/app/components/inicioadmin/barra-lateral/barra-lateral.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-barra-lateral',
+  imports: [MatIcon, RouterLink, RouterLinkActive],
+  templateUrl: './barra-lateral.html',
+  styleUrl: './barra-lateral.css',
+})
+export class BarraLateral {}
