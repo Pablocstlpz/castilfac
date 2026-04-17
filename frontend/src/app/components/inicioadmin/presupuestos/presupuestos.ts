e2dd9cb04f3554a83f21416b5286bb0bd4b6bf30
Pablo M. Del Castillo López <pmcastillo@deuser.es>
2026-04-17 13:05:41 +0200

    feat: agregar importación de RouterLink en el componente de presupuestos

--- frontend/src/app/components/inicioadmin/presupuestos/presupuestos.ts
+++ frontend/src/app/components/inicioadmin/presupuestos/presupuestos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { MatIcon } from "@angular/material/icon";
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-presupuestos',
@@ -7,6 +8,4 @@ import { MatIcon } from "@angular/material/icon";
   templateUrl: './presupuestos.html',
   styleUrl: './presupuestos.css',
 })
-export class Presupuestos {
-
-}
+export class Presupuestos {}
