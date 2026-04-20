98cd5c2e7021430112d8e5a249861dc4102e1e8b
Pablo M. Del Castillo López <pmcastillo@deuser.es>
2026-04-20 14:57:27 +0200

    feat: agregar componente y plantilla para el detalle de cliente

---
+++ frontend/src/app/components/inicioadmin/clientes/detalle-cliente/detalle-cliente.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
+
+@Component({
+  selector: 'app-detalle-cliente',
+  imports: [MatIcon],
+  templateUrl: './detalle-cliente.html',
+  styleUrl: './detalle-cliente.css',
+})
+export class DetalleCliente {
+
+}
