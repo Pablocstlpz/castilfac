4c7b3a33824a55045bc8d2362f757f016c4fde3a
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-04-22 18:47:23 +0200

    feat: agregar servicios para obtener elementos, elementos materiales, historial de precios base y historial de precios de empresa

---
+++ frontend/src/app/services/historial-precios-base.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../environments/enviroments';
+import { HistorialPrecioBase } from '../interfaces/historial-precio-base';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HistorialPreciosBase {
+  private URL = environment.apiUrl;
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      //  'Authorization': `Bearer ${this.token}`
+    }),
+  };
+
+  private http = inject(HttpClient);
+
+  //obtener todos los elementos
+  getHistorialPreciosBase(): Observable<HistorialPrecioBase[]> {
+    return this.http.get<HistorialPrecioBase[]>(`${this.URL}/historialPreciosBase`).pipe(
+      map((response) => response), // Aseguramos que la respuesta se trate como un array de HistorialPrecioBase
+      catchError(this.handleError),
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    const errorMessage = error.error?.message || 'Error desconocido al procesar la solicitud';
+
+    return throwError(() => new Error(errorMessage));
+  }
+}
