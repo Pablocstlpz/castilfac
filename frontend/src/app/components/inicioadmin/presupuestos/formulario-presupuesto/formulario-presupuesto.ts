5f648e1c18ed88b5d6c63d9c6beaf7dd47408169
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-05 14:50:51 +0200

    feat: agregar componente formulario-presupuesto con su HTML y CSS

---
+++ frontend/src/app/components/inicioadmin/presupuestos/formulario-presupuesto/formulario-presupuesto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-presupuesto',
+  imports: [],
+  templateUrl: './formulario-presupuesto.html',
+  styleUrl: './formulario-presupuesto.css',
+})
+export class FormularioPresupuesto {
+
+}
