0af94c62c674d3292941bf44f85c269060f3db67
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-06 14:07:55 +0200

    feat: agregar componente VerificacionPerfil con su HTML, CSS y pruebas

---
+++ frontend/src/app/components/verificacion-perfil/verificacion-perfil.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificacion-perfil',
+  imports: [],
+  templateUrl: './verificacion-perfil.html',
+  styleUrl: './verificacion-perfil.css',
+})
+export class VerificacionPerfil {
+
+}
