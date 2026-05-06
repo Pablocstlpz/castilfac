f36baa84aa9c7dc0b0371044b6e5f2b20bdfad00
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-06 14:07:34 +0200

    feat: agregar componente RestablecerPassword con su HTML, CSS y pruebas

---
+++ frontend/src/app/components/restablecer-password/restablecer-password.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restablecer-password',
+  imports: [],
+  templateUrl: './restablecer-password.html',
+  styleUrl: './restablecer-password.css',
+})
+export class RestablecerPassword {
+
+}
