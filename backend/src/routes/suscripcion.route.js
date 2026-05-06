d5333266339a1b6dc26dde5907b0cab3c3a86daa
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-06 14:30:48 +0200

    feat: agregar ruta para verificar suscripción de empresa

---
+++ backend/src/routes/suscripcion.route.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import { Router } from 'express';
+import { verificarSuscripcion } from '../controllers/suscripcion.controller.js';
+
+const router = Router();
+
+router.get('/suscripcion/check/:empresa_id', verificarSuscripcion);
+
+export { router as suscripcionRoutes };
