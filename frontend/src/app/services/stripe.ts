1aebaf6c3ff64c7772923920a38aa85fa1e19b66
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-07 14:26:27 +0200

    feat: agregar servicio Stripe con métodos para crear y verificar sesiones de pago

---
+++ frontend/src/app/services/stripe.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+import { catchError, Observable, throwError } from 'rxjs';
+
+import { environment } from '../../environments/enviroments';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StripeServices {
+  private URL = environment.apiUrl;
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      //  'Authorization': `Bearer ${this.token}`
+    }),
+  };
+
+  private http = inject(HttpClient);
+
+  crearSesionCheckout(empresa_id: number): Observable<{ url: string }> {
+    return this.http
+      .post<{ url: string }>(`${this.URL}/stripe/crear-sesion`, { empresa_id }, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  verificarSesionPago(session_id: string): Observable<{ message: string }> {
+    return this.http
+      .post<{ message: string }>(
+        `${this.URL}/stripe/verificar-sesion`,
+        { session_id },
+        this.httpOptions,
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    const errorMessage = error.error?.message || 'Error desconocido al procesar la solicitud';
+
+    return throwError(() => new Error(errorMessage));
+  }
+}
