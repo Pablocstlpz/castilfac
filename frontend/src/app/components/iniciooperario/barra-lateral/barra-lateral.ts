1a3c162e3f02e789049867307b0a099195b18f55
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-12 14:18:26 +0200

    feat: mejorar barra lateral con cierre de menú móvil y ajustes en la estructura

--- frontend/src/app/components/iniciooperario/barra-lateral/barra-lateral.ts
+++ frontend/src/app/components/iniciooperario/barra-lateral/barra-lateral.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
@@ -14,10 +14,17 @@ export class BarraLateral {
   private authentication = inject(Authentication);
   private router = inject(Router);
 
+  @Input() menuMovilAbierto = false;
+  @Output() cerrarMenu = new EventEmitter<void>();
+
   public usuario = this.authentication.obtenerUsuarioSesion();
 
   cerrarSesion() {
     this.authentication.cerrarSesion();
     this.router.navigate(['/sesioncerrada']);
+  }
+
+  cerrarMenuMovil() {
+    this.cerrarMenu.emit();
   }
 }
