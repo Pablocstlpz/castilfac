e5b68bfe9987dbb9620f6263b9edfacd965643c7
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-12 14:11:43 +0200

    fix: unificar comillas en imports del archivo app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Welcome } from "./components/enlaces-generales/welcome/welcome";
-import { Head } from "./components/partes-html/head/head";
-import { Footer } from "./components/partes-html/footer/footer";
+import { Welcome } from './components/enlaces-generales/welcome/welcome';
+import { Head } from './components/partes-html/head/head';
+import { Footer } from './components/partes-html/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Head, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
