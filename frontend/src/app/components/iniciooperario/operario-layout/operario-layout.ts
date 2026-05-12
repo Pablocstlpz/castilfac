b4fc8f5403890e1280bf41769fbedcbc1ef4802f
Pablo M. Del Castillo López <pablomanueldcl93@gmail.com>
2026-05-12 14:18:44 +0200

    feat: agregar funcionalidad de menú móvil con apertura y cierre en el layout del operario

--- frontend/src/app/components/iniciooperario/operario-layout/operario-layout.ts
+++ frontend/src/app/components/iniciooperario/operario-layout/operario-layout.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { BarraLateral } from "../barra-lateral/barra-lateral";
 import { RouterModule } from "@angular/router";
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-operario-layout',
-  imports: [BarraLateral, RouterModule],
+  imports: [BarraLateral, RouterModule, MatIcon],
   templateUrl: './operario-layout.html',
   styleUrl: './operario-layout.css',
 })
 export class OperarioLayout {
+  public menuMovilAbierto = false;
+
+  abrirMenuMovil() {
+    this.menuMovilAbierto = true;
+  }
+
+  cerrarMenuMovil() {
+    this.menuMovilAbierto = false;
+  }
 
 }
