d2248e5296d3480cfdceec39e48d5af36852cb61
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-05-13 22:32:01 +0200

    fix: add RouterLink import to feature component for improved navigation

--- frontend/src/app/components/enlaces-generales/feature/feature.ts
+++ frontend/src/app/components/enlaces-generales/feature/feature.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-feature',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './feature.html',
   styleUrl: './feature.css',
 })
